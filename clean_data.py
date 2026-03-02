952de4e616173344d601c99065faae5700099344
brigittewylee <brigitte.leewy@gmail.com>
2026-03-01 21:41:29 -0500

    script to add user countries

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("universal_top_spotify_songs.csv")
+# pull out relevant variables
+df = pd.read_csv("datasets/universal_top_spotify_songs.csv")
 
 print(df.columns)
 
@@ -15,5 +16,21 @@ df2 = df1[
     ["spotify_id", "name", "artists", "country", "daily_rank", "snapshot_date", "month"]
 ]
 
-df2.to_csv("cleaned_data.csv")
-test = "apple"
+df2.to_csv("datasets/cleaned_data.csv")
+
+
+# use borda rank to create monthly rankings
+df = pd.read_csv("datasets/cleaned_data.csv")
+countries = df["country"].unique()
+
+df["borda_score"] = 51 - df["daily_rank"]
+df = df.dropna()
+
+ranked_borda = df.groupby(['country', 'month', 'name', 'artists'])['borda_score'].sum().reset_index()
+ranked_borda = ranked_borda.sort_values(['month', 'country', 'borda_score'], ascending=[True, True, False])
+
+monthly_top = ranked_borda.groupby(['country', 'month']).head(10).reset_index()
+monthly_top.columns
+monthly_top['month'].unique
+
+monthly_top.to_csv("monthly_top.csv")
